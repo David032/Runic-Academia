60fe508a5b35480d5632eba46904c7d190aea0ac
David K <davek032@outlook.com>
2021-10-19 13:05:01 +0100

    More adjustor work

---
+++ Assets/Cardinal/Adjustor/Responses/DungeonProgressionResponse.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Cardinal.Adjustor
+{
+    public class DungeonProgressionResponse : Response
+    {
+        public override void Execute()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+
+}
---
+++ Assets/Cardinal/Adjustor/Responses/KillDeathResponse.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Cardinal.Adjustor
+{
+    public class KillDeathResponse : Response
+    {
+        public override void Execute()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void ExecuteIncrease() 
+        {
+        
+        }
+
+        public void ExecuteDecrease() 
+        {
+        
+        }
+    }
+
+}
